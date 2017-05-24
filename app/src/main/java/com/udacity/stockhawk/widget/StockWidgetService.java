e5e8d559092d14a0d01f542709cbe306bfcc218c
paschalidis <msf7415012@teilar.gr>
2017-05-24 20:21:38 +0300

    feat: widget service remote views

--- app/src/main/java/com/udacity/stockhawk/widget/StockWidgetService.java
+++ app/src/main/java/com/udacity/stockhawk/widget/StockWidgetService.java
@@ -1,11 +1,108 @@
 package com.udacity.stockhawk.widget;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.os.Binder;
+import android.widget.AdapterView;
+import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
+
+import com.udacity.stockhawk.R;
+import com.udacity.stockhawk.data.Contract.Quote;
 
 public class StockWidgetService extends RemoteViewsService {
     @Override
     public RemoteViewsFactory onGetViewFactory(Intent intent) {
-        return null;
+        return new RemoteViewsFactory() {
+
+            private Cursor data = null;
+
+            @Override
+            public void onCreate() {
+
+            }
+
+            @Override
+            public void onDataSetChanged() {
+                if (data != null) {
+                    data.close();
+                }
+
+                // This method is called by the app hosting the widget (e.g., the launcher)
+                // However, our ContentProvider is not exported so it doesn't have access to the
+                // data. Therefore we need to clear (and finally restore) the calling identity so
+                // that calls use our process and permission
+                final long identityToken = Binder.clearCallingIdentity();
+
+                data = getContentResolver().query(Quote.URI,
+                        null,
+                        null,
+                        null,
+                        null);
+                Binder.restoreCallingIdentity(identityToken);
+            }
+
+            @Override
+            public void onDestroy() {
+                if (data != null) {
+                    data.close();
+                    data = null;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return data == null ? 0 : data.getCount();
+            }
+
+            @Override
+            public RemoteViews getViewAt(int position) {
+                if (position == AdapterView.INVALID_POSITION
+                        || data == null
+                        || !(data.moveToPosition(position))) {
+                    return null;
+                }
+
+                RemoteViews views = new RemoteViews(getPackageName(),
+                        R.layout.list_item_quote);
+
+                //Get stock data from cursor
+                int stockId = data.getInt(Quote.POSITION_ID);
+                String symbol = data.getString(Quote.POSITION_SYMBOL);
+                String price = data.getString(Quote.POSITION_PRICE);
+                String change = data.getString(Quote.POSITION_ABSOLUTE_CHANGE);
+                String history = data.getString(Quote.POSITION_HISTORY);
+
+                //Set stock data to view
+                views.setTextViewText(R.id.symbol, symbol);
+                views.setTextViewText(R.id.price, price);
+                views.setTextViewText(R.id.change, change);
+
+                return views;
+            }
+
+            @Override
+            public RemoteViews getLoadingView() {
+                return new RemoteViews(getPackageName(), R.layout.list_item_quote);
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 1;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                if (data.moveToPosition(position)) {
+                    return data.getLong(Quote.POSITION_ID);
+                }
+                return position;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return false;
+            }
+        };
     }
 }
